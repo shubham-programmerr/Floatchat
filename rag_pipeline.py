9c0ee15fd2a1189709f05d83bcba0f45245a0cad
Shubham dadhich <dadhichshubham32@gmail.com>
2025-09-22 23:47:54 +0530

    Add files via upload

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -10,7 +10,7 @@ import numpy as np
 
 # --- Configuration ---
 DB_CONNECTION_STRING = st.secrets["connections"]["postgres"]["url"]
-GOOGLE_API_key = st.secrets["AIzaSyBEpSu05Z3jeroEHB4eti15DCSEJfovqc0"]
+GOOGLE_API_key = st.secrets["AIzaSyCM2s5PztPtXGI57z7sEtHY2pM0Nvm9ntw"]
 
 # --- Initialize Models and Database Connection ---
 llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_key)
